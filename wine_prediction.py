599f0d07a9a63736fdc011daf92e33fcd2f634ff
Vladimir <limtis2109@gmail.com>
2022-05-08 19:18:44 +0200

    Data cleaning tweaks

--- wine_prediction.py
+++ wine_prediction.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import pickle
 import numpy as np
+import pickle
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 
 # Replaces values that are not common (<thresh)
@@ -11,16 +11,21 @@ def column_replace_uncommon(col_name, val_thresh, replace_name):
 
 
 if __name__ == '__main__':
+    # DATA OPERATIONS
     pd.set_option('display.max_columns', None)
 
     df = pd.read_csv('./wines_SPA.csv')
-    # Remove unused info
-    df.drop(['num_reviews', 'country'], axis=1, inplace=True)
+
+    df.drop(['num_reviews', 'wine', 'country'], axis=1, inplace=True)  # Remove not-usable info
+
+    df['year'] = df['year'].replace('N.V.', np.nan)  # Remove Non-Vintage wines and Na's
     df = df.dropna()
+    df['year'] = df['year'].astype(np.int64)
 
     # Replace uncommon values with 'other'
-    column_replace_uncommon('winery', 30, 'other')
-    column_replace_uncommon('wine', 20, 'other')
+    column_replace_uncommon('winery', 20, 'other')
     column_replace_uncommon('region', 10, 'other')
 
-    print(df.head(5))
+    # MODEL TRAINING
+    X = df.drop(columns=['rating'])
+    y = df['rating']
