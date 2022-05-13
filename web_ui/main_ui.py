319d80467139b5593b64781e992fa3ff6e7b2238
juzikaster <izax4321@gmail.com>
2022-05-13 14:41:45 +0200

    fix

--- web_ui/main_ui.py
+++ web_ui/main_ui.py
@@ -50,8 +50,8 @@ def get_result():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=3090)
 
 
 def run_app():
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=3090)
